af98886e2593bd356b52a9f7caf58057c865a0d8
James Osunkiyesi <jaydhales@gmail.com>
2023-12-08 16:46:11 +0100

    Updates: Added new function

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -3,10 +3,30 @@ pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 
+import "../src/ImpactDAO.sol";
+import "../src/ImpactDAOToken.sol";
+import "../src/ImpactRewardToken.sol";
+
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        address Admin = msg.sender;
+
+        ImpactDAOToken Token = new ImpactDAOToken(Admin, "token Uri");
+        ImpactRewardsToken rewardee = new ImpactRewardsToken();
+        ImpactDAO impactDao = new ImpactDAO(Admin, address(Token), address(rewardee));
+
+        console2.logAddress(msg.sender);
+        console2.logAddress(Admin);
+        console2.logAddress(address(Token));
+        console2.logAddress(address(rewardee));
+        console2.logAddress(address(impactDao));
+
+        vm.stopBroadcast();
     }
 }
+//  0xa2b058528741AFC93631B26F1f71546dAa1B7f34 // TOKEN
+//   0x9e609f87336adf22D19E573417cf00e44Bec194a // REWARD
+//   0xcD81aFBAb25A58381B7DDcC1884839Ea26eAE784 // DAO
